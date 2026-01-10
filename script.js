bf2d9a1c29dc47a4e863c443d60c590546e3c9e7
sliman1012 <slimansh1999@gmail.com>
2026-01-10 18:46:58 +0200

    sec_pro

---
+++ script.js
@@ -0,0 +1,41 @@
+/*
+///const word=" js is running "
+///console.log(word);
+const word2=" MY NAME IS "
+///console.log(word2)
+const num =2+2 
+console.log(num);
+const bool="true"
+console.log(true);
+const word3=" done"
+console.log(word3);
+console.log("Hello, Dana!")
+let a=10
+let b=20 
+console.log("Total: " , a + b);
+const Goaloutput="im 18 years old " ;
+console.log(Goaloutput) 
+const num4 =10;
+if ( num4 >10 )/*/
+const num = 10;
+if( num>= 10 )
+    console.log( "positive ")
+else if( num > 10 )
+console.log("negative ")
+else(num!= 10)
+    console.log("zero")
+
+    const pass= 1234;
+    if( pass==1234) 
+        console.log(" equals ")
+
+
+
+
+
+
+
+
+
+
+
